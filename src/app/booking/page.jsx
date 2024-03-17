1b58d00a713fa382db2b4b7f14e56ed49ed85ef1
Ahsinal <lanishakoirala23@gmail.com>
2024-03-17 14:39:39 +0545

    added new booking page

--- src/app/booking/page.jsx
+++ src/app/booking/page.jsx
@@ -1,23 +1,34 @@
-import React from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+"use client";
+import { useState } from "react";
+import { Button, Col, Container, Form, Row, InputGroup } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 const Booking = () => {
-  const { register } = useForm();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
+  const [formData, setFormData] = useState({});
+  const onSubmit = (data) => {
+    console.log("Form Data submitted:", data);
+    setFormData(data);
+    reset();
+  };
   return (
     <>
-      <Container>
-        Booking
-        <Row className="gap-8-row mt-8 ">
-          <Col lg={12} sm={12}>
-            {/* <form>
+      <Container className="py-40">
+        <Row className="gap-8-row mt-8 flex-center-center">
+          <Col lg={6} sm={12} className="rounded-12 shadow p-32">
+            <Form>
               <div className="mb-4">
-                <label htmlFor="" className="form-label">
+                <Form.Label htmlFor="" className="form-label mb-4">
                   Full Name
                   {errors.name?.type === "required" && (
                     <span className="text-secondary ms-4">*</span>
                   )}
-                </label>
-                <input
+                </Form.Label>
+                <Form.Control
                   {...register("name", {
                     required: true,
                     pattern: {
@@ -28,6 +39,7 @@ const Booking = () => {
                   })}
                   aria-invalid={errors.name ? "true" : "false"}
                   type="text"
+                  required
                 />
                 {errors.name?.type === "required" && (
                   <p className="small text-secondary" role="alert">
@@ -42,13 +54,13 @@ const Booking = () => {
               </div>
 
               <div className="mb-4">
-                <label htmlFor="" className="form-label">
+                <Form.Label htmlFor="" className="form-label mb-4">
                   Email
                   {errors.email?.type === "required" && (
                     <span className="text-secondary ms-4">*</span>
                   )}
-                </label>
-                <input
+                </Form.Label>
+                <Form.Control
                   {...register("email", {
                     required: true,
                     pattern: {
@@ -71,13 +83,13 @@ const Booking = () => {
               </div>
 
               <div className="mb-4">
-                <label htmlFor="" className="form-label">
+                <Form.Label htmlFor="" className="form-label mb-4">
                   Phone{" "}
                   {errors.phone?.type === "required" && (
                     <span className="text-secondary ms-4">*</span>
                   )}
-                </label>
-                <input
+                </Form.Label>
+                <Form.Control
                   {...register("phone", {
                     required: true,
                     pattern: {
@@ -99,15 +111,42 @@ const Booking = () => {
                   </p>
                 )}
               </div>
-
-              <div className="mb-8">
-                <label htmlFor="" className="form-label">
-                  Message{" "}
+              <div className="mb-4">
+                <Form.Label htmlFor="" className="form-label mb-4">
+                  Country
+                  {errors.country?.type === "required" && (
+                    <span className="text-secondary ms-4">*</span>
+                  )}
+                </Form.Label>
+                <Form.Select
+                  {...register("country", { required: true })}
+                  className="form-select mb-8"
+                  aria-label="country"
+                  aria-invalid={errors.country ? "true" : "false"}
+                >
+                  <option value="" className="xx-small">
+                    Select a country
+                  </option>
+                  <option value="1">America</option>
+                  <option value="2">Australia 2</option>
+                  <option value="3">Antartica</option>
+                </Form.Select>
+                {errors.country?.type === "required" && (
+                  <p className="small text-secondary" role="alert">
+                    Country selection is required
+                  </p>
+                )}
+              </div>
+              <div className="mb-4">
+                <Form.Label htmlFor="" className="form-label mb-4">
+                  Message
                   {errors.message?.type === "required" && (
                     <span className="text-secondary ms-4">*</span>
                   )}
-                </label>
-                <textarea
+                </Form.Label>
+                <Form.Control
+                  as="textarea"
+                  resize="none"
                   {...register("message", { required: true })}
                   aria-invalid={errors.message ? "true" : "false"}
                 />
@@ -117,7 +156,14 @@ const Booking = () => {
                   </p>
                 )}
               </div>
-            </form> */}
+            </Form>
+            <Button
+              className="btn-xs mt-16"
+              type="submit"
+              onClick={handleSubmit(onSubmit)}
+            >
+              Submit
+            </Button>
           </Col>
         </Row>
       </Container>
