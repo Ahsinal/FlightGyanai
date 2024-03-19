e8e49cac50bf2f2d13cf22c715156eb5c55fa2f8
Ahsinal <lanishakoirala23@gmail.com>
2024-03-19 13:20:39 +0545

    added 4o4 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { Container } from "react-bootstrap";
+
+const NotFound = () => {
+  return (
+    <>
+      <section>
+        <Container className="py-40">
+          <Link href="/" className="text-secondary">Back To Home</Link>
+          <div className="img-landscape">
+            <img
+              src="https://cdn.pixabay.com/photo/2016/03/12/14/19/error-404-1252056_1280.png"
+              alt="4o4 Not found"
+            />
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default NotFound;
