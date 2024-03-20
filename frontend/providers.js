03b66471a1f56ae39770bfe806ed195b7f4e4e4a
Ahsinal <lanishakoirala23@gmail.com>
2024-03-20 09:43:18 +0545

    testing ssh

--- frontend/providers.js
+++ frontend/providers.js
@@ -1,12 +1,12 @@
 "use client"
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { SSRProvider } from "react-bootstrap";
+
 
 export function Providers({ children }) {
     return (
         <Provider store={store}>
-            <SSRProvider>{children}</SSRProvider>
+            {children}
         </Provider>
     );
 }
